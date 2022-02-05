4590031bd4da189654add62518d0880cfd2c9216
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-02-05 12:55:22 -0300

    Criação do Header da página

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
+import PageHeader from "../template/pageHeader";
+
 export default class Todo extends Component {
     render() {
         return (
             <div>
                 <h1>
-                    Todo
+                    <PageHeader name='Tarefas' small='Cadastro' />
                 </h1>
             </div>
         )
