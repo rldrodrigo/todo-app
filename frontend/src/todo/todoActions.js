d583a744632c60585d39a768fc087751e8631b88
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-03-23 05:05:41 -0300

    Implementação da Exclusão de TODOs

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -37,3 +37,9 @@ export const markAsPending = (todo) => {
     }
 }
 
+export const remove = (todo) => {
+    return dispatch => {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => dispatch(search()))
+    }
+}
